29c87ce02fe29543f36eb4ae7f55e60f97d27ef5
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2022-10-18 13:04:20 -0400

    React project code added

---
+++ build/precache-manifest.4cfcd0b6fc91168ee469a1b6456d369c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cca8547a7463991f14f1",
+    "url": "/MohammedVep/comp3104_lab_f2022/static/css/main.f8c94020.chunk.css"
+  },
+  {
+    "revision": "cca8547a7463991f14f1",
+    "url": "/MohammedVep/comp3104_lab_f2022/static/js/main.cca8547a.chunk.js"
+  },
+  {
+    "revision": "c07fce4ac11782f42e84",
+    "url": "/MohammedVep/comp3104_lab_f2022/static/js/runtime~main.c07fce4a.js"
+  },
+  {
+    "revision": "b1773f8afd9fb6a0106e",
+    "url": "/MohammedVep/comp3104_lab_f2022/static/js/2.b1773f8a.chunk.js"
+  },
+  {
+    "revision": "ba4a61920f2d82772ca2ed8879b14ab1",
+    "url": "/MohammedVep/comp3104_lab_f2022/index.html"
+  }
+];
